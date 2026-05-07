e88bdec8dc48320c631ecfa3bd8ff29ce07b5ee5
Ram2389 <bel89omar@live.it>
2026-05-07 16:19:58 +0200

    update

--- ci-cd-/calculator/calculator.py
+++ ci-cd-/calculator/calculator.py
@@ -1,18 +1,23 @@
-#calculator for the CI-CD
+# calculator for the CI-CD
+
 
 def add(a: float, b: float) -> float:
     return a + b
 
+
 def subtract(a: float, b: float) -> float:
     return a - b
 
+
 def multiply(a: float, b: float) -> float:
     return a * b
+
 
 def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
 
 if __name__ == "__main__":
     print("Operazioni disponibili: +, -, *, /")
--- ci-cd-/tests/test_calculator.py
+++ ci-cd-/tests/test_calculator.py
@@ -1,24 +1,29 @@
 from calculator.calculator import add, subtract, multiply, divide
 import pytest
 
+
 def test_add():
     assert add(1, 1) == 2
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
+
 
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
 
+
 def test_multiply():
     assert multiply(3, 3) == 9
     assert multiply(0, 5) == 0
     assert multiply(-2, 3) == -6
 
+
 def test_divide():
     assert divide(10, 2) == 5
     assert divide(0, 1) == 0
+
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
